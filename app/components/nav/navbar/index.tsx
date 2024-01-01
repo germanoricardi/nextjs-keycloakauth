84b628ef0394d48e99868f1c42d4ccd87ac4672a
Germano Ricardi <germanoricardi7@gmail.com>
2024-01-01 18:19:30 -0400

    Adicionado nome de usuário no navbar;

--- app/components/nav/navbar/index.tsx
+++ app/components/nav/navbar/index.tsx
@@ -1,13 +1,25 @@
+'use client';
+
+import { signIn, signOut, useSession } from "next-auth/react";
 import Link from "next/link"
 
 export const Navbar = () => {
+  const { data: session, status } = useSession();
+
   return (
     <nav className="navbar bg-body-tertiary">
       <div className="container">
           <Link href={'/'} className="navbar-brand">KC Auth</Link>
           
           <div className="d-flex">
-            <button className="btn btn-outline-success btn-sm" type="submit">Login</button>
+            {
+              (status == 'authenticated')
+              ? <>
+                  <button className="btn btn-sm btn-link">{session?.user?.name}</button>
+                  <button className="btn btn-outline-success btn-sm" type="submit" onClick={() => signOut({ callbackUrl: '/' })}>Logout</button>
+                </>
+              : <button className="btn btn-outline-success btn-sm" type="submit" onClick={() => signIn()}>Login</button>
+            }
           </div>
         </div>
     </nav>
